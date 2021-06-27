85c30dbe39c397a6bc77bbb18954a52c1d9bf122
Anwesh43 <anweshthecool0@gmail.com>
2021-06-28 03:10:34 +0530

    adding constants and libraries

--- rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
+++ rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
@@ -1,2 +1,25 @@
 package com.example.rectendlineview
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Color
+import android.graphics.Canvas
+import android.app.Activity
+import android.content.Context
+
+val parts : Int = 4
+val scGap : Float = 0.02f / parts
+val strokeFactor : Float = 90f
+val sizeFactor : Float = 3.2f
+val delay : Long = 20
+val colors : Array<Int> = arrayOf(
+    "#f44336",
+    "#004D40",
+    "#FFD600",
+    "#00C853",
+    "#6200EA"
+).map {
+    Color.parseColor(it)
+}.toTypedArray()
+val backColor : Int = Color.parseColor("#BDBDBD")
