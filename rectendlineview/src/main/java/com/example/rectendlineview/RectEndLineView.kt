f0c5813c856aa8e7c0819f142cc755ede4a1cd1d
Anwesh43 <anweshthecool0@gmail.com>
2021-06-28 03:37:16 +0530

    adding RectEdgeLine class

--- rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
+++ rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
@@ -171,5 +171,28 @@ class RectEndLineView(ctx : Context) : View(ctx) {
                 return this
             }
         }
+
+        data class RectEndLine(var i : Int) {
+
+            private var curr : RELNode = RELNode(0)
+            private var dir : Int = 1
+
+            fun draw(canvas : Canvas, paint : Paint) {
+                curr.draw(canvas, paint)
+            }
+
+            fun update(cb : (Float) -> Unit) {
+                curr.update {
+                    curr = curr.getNext(dir) {
+                        dir *= -1
+                    }
+                    cb(it)
+                }
+            }
+
+            fun startUpdating(cb : () -> Unit) {
+                curr.startUpdating(cb)
+            }
+        }
     }
 }
