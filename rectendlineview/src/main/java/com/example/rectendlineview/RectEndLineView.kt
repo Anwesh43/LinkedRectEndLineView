c0a6d1e7aee99e75ccc5ed67d14568980aefe094
Anwesh43 <anweshthecool0@gmail.com>
2021-06-28 03:39:16 +0530

    adding Renderer class

--- rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
+++ rectendlineview/src/main/java/com/example/rectendlineview/RectEndLineView.kt
@@ -194,5 +194,28 @@ class RectEndLineView(ctx : Context) : View(ctx) {
                 curr.startUpdating(cb)
             }
         }
+
+        data class Renderer(var view : RectEndLineView) {
+
+            private val animator : Animator = Animator(view)
+            private val rel : RectEndLine = RectEndLine(0)
+            private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+            fun render(canvas : Canvas) {
+                canvas.drawColor(backColor)
+                rel.draw(canvas, paint)
+                animator.animate {
+                    rel.update {
+                        animator.stop()
+                    }
+                }
+            }
+
+            fun handleTap() {
+                rel.startUpdating {
+                    animator.start()
+                }
+            }
+        }
     }
 }
